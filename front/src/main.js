02a70fd0920523cfc9da249460576ef9d0c94105
pukeeee <pukee18@ukr.net>
2025-01-24 22:58:21 +0200

    swipe buttons

--- front/src/main.js
+++ front/src/main.js
@@ -1,15 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vue3TouchEvents from "vue3-touch-events";
 
 const app = createApp(App)
 
-if (window.Telegram?.WebApp) {
+if (window.Telegram?.WebApp && window.Telegram.WebApp.disableVerticalSwipes) {
     window.Telegram.WebApp.ready(); // Сообщаем что WebApp готов
     window.Telegram.WebApp.expand(); // Делаем окно по всей высоте
+    window.Telegram.WebApp.disableVerticalSwipes();
 }
 
 app.use(router)
-
+app.use(Vue3TouchEvents, {
+    swipeThreshold: 50, // Минимальное расстояние для срабатывания свайпа
+    touchHoldTolerance: 500, // Длительность удержания до свайпа
+});
 app.mount('#app')
 
